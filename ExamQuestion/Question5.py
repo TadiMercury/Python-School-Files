902a5846b8e166bd3ae1ca326c9abb2b70b0e73c
TadiMercury <magara.tadiwa9@gmail.com>
2020-08-19 22:40:05 +0200

    Question Ans

--- ExamQuestion/Question5.py
+++ ExamQuestion/Question5.py
@@ -1,11 +1,17 @@
 CDTitle = ''
 CDArstist = ''
 CDLocation = ''
+TextLine = ''
 
-def InputData (CDTitle,CDArstist,CDLocation,TextLine):
-    CDTitle = input("Input CD Title: ")
-    CDArstist = input("Input CD Artist: ")
-    CDLocation = input("Input CD Location: ")
+#def InputData (CDTitle,CDArstist,CDLocation,TextLine):
+CDTitle = input("Input CD Title: ")
+CDArstist = input("Input CD Artist: ")
+CDLocation = input("Input CD Location: ")
+TextLine = CDTitle + CDArstist + CDLocation
 
-    TextLine = CDTitle + CDArstist + CDLocation
+file_path = "SongFile.txt"
+file = open(file_path,"w")
+file.write(TextLine)
+file.close()
+print(TextLine)
     
